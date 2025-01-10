e18e4230f0381ed332b6acb17392f0e6586d0f29
Peer Stelldinger <69846153+PStelldinger@users.noreply.github.com>
2025-01-10 08:08:23 +0100

    Added setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='puzzle_board',
+      version='1.0',
+      description='',
+      author='Peer Stelldinger',
+      author_email='peer.stelldinger@haw-hamburg.de',
+      packages=['puzzle_board', 'puzzle_board.cornerdetection'],
+      )
