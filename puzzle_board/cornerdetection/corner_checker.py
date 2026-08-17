2219416bd9de86f45a302c03d945ec1396d84e5c
TreeKid <jurizach@gmail.com>
2026-08-17 10:38:12 +0200

    small code cleanup

--- puzzle_board/cornerdetection/corner_checker.py
+++ puzzle_board/cornerdetection/corner_checker.py
@@ -75,20 +75,23 @@ class CornerChecker:
         ]
         self.p = 1
 
+        # placeholder for memory allocation
         self.outer_filters_array = None
         self.inner_filters_array = None
+
+    def allocate_memory(self, image_size):
+        self.outer_filters_array = np.zeros((len(self.outer_filter), *image_size))
+        self.inner_filters_array = np.zeros((len(self.inner_filter), *image_size))
 
     def filter_corners(self, image: np.ndarray) -> np.ndarray:
 
         # reserve memory for the filtered images
         if self.outer_filters_array is None or self.outer_filters_array.shape[:2] != image.shape[:2]:
-            self.outer_filters_array = np.zeros((len(self.outer_filter), *image.shape[:2]))
-        if self.inner_filters_array is None or self.inner_filters_array.shape[:2] != image.shape[:2]:
-            self.inner_filters_array = np.zeros((len(self.inner_filter), *image.shape[:2]))
+            self.allocate_memory(image.shape[:2])
 
+        # filter the image with outer and inner filters
         for i, f in enumerate(self.outer_filter):
             cv2.filter2D(image, -1, f, dst=self.outer_filters_array[i])
-
         for i, f in enumerate(self.inner_filter):
             cv2.filter2D(image, -1, f, dst=self.inner_filters_array[i])
 
@@ -96,7 +99,7 @@ class CornerChecker:
         max_result1 = self.p * np.max(np.abs(self.outer_filters_array), axis=0) / 2
         max_result2 = np.max(np.abs(self.inner_filters_array), axis=0)
 
-        final_result = np.maximum(image * 0, max_result1 - max_result2)
+        final_result = np.maximum(0, max_result1 - max_result2)
         # print(np.min(final_result))
         # print(np.max(final_result))
         return final_result
