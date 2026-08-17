9aeab7c543768e99983444eaefb9667e8d2e29a6
TreeKid <jurizach@gmail.com>
2026-08-17 10:09:58 +0200

    pre-allocate arrays for corner checker filters

--- puzzle_board/cornerdetection/corner_checker.py
+++ puzzle_board/cornerdetection/corner_checker.py
@@ -75,17 +75,29 @@ class CornerChecker:
         ]
         self.p = 1
 
-    def filter_corners(self, image: np.ndarray) -> np.ndarray:
-        results1 = [cv2.filter2D(image, -1, f) for f in self.outer_filter]
-        outer_filters_concatenated = np.stack(results1, axis=-1)
+        self.outer_filters_array = None
+        self.inner_filters_array = None
 
-        # Taking max along the 3rd axis and multiplying by p/2
-        max_result1 = self.p * np.max(np.abs(outer_filters_concatenated), axis=-1) / 2
-        results2 = [cv2.filter2D(image, -1, f) for f in self.inner_filter]
-        inner_filters_concatenated = np.stack(results2, axis=-1)
-        max_result2 = np.max(np.abs(inner_filters_concatenated), axis=-1)  # Taking max along the 3rd axis
+    def filter_corners(self, image: np.ndarray) -> np.ndarray:
+
+        # reserve memory for the filtered images
+        if self.outer_filters_array is None or self.outer_filters_array.shape[:2] != image.shape[:2]:
+            self.outer_filters_array = np.zeros((len(self.outer_filter), *image.shape[:2]))
+        if self.inner_filters_array is None or self.inner_filters_array.shape[:2] != image.shape[:2]:
+            self.inner_filters_array = np.zeros((len(self.inner_filter), *image.shape[:2]))
+
+        for i, f in enumerate(self.outer_filter):
+            cv2.filter2D(image, -1, f, dst=self.outer_filters_array[i])
+
+        for i, f in enumerate(self.inner_filter):
+            cv2.filter2D(image, -1, f, dst=self.inner_filters_array[i])
+
+        # Taking max along axis=0 and multiplying by p/2
+        max_result1 = self.p * np.max(np.abs(self.outer_filters_array), axis=0) / 2
+        max_result2 = np.max(np.abs(self.inner_filters_array), axis=0)
 
         final_result = np.maximum(image * 0, max_result1 - max_result2)
-        #print(np.min(final_result))
-        #print(np.max(final_result))
+        # print(np.min(final_result))
+        # print(np.max(final_result))
         return final_result
+
