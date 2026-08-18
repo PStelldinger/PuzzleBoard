fb29e479c9823a2cb412b4fc7952d2c299aeda64
TreeKid <jurizach@gmail.com>
2026-08-18 10:11:15 +0200

    seprate filter images

--- puzzle_board/puzzle_board_detector.py
+++ puzzle_board/puzzle_board_detector.py
@@ -38,24 +38,14 @@ filter_dtype=np.float32
 h_d = HessianDetector(dtype=filter_dtype)
 corner_checker=CornerChecker(dtype=filter_dtype)
 
-def _detect_puzzles(img, min_width, curved, decoder):
 
-    global filter_dtype, h_d, corner_checker
+def _filter_image(img):
 
-    assert filter_dtype in [np.float32, np.float64], 'Currently only float dtype is supported'
-
-    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY).astype(filter_dtype)
-    gray = gray/255
-
-# For low resolution images, there should be less blurring:
-#    img2 = cv2.filter2D(src=gray, ddepth=-1, kernel=np.array([[0,1,0],[1,1,1],[0,1,0]]))
-    img2 = cv2.GaussianBlur(gray, (3, 3), 1)
-
-    h_d.filter_image(img2)
-    profile, mS = h_d.detect_corners(img2, k=1.0)
+    h_d.filter_image(img)
+    profile, mS = h_d.detect_corners(img, k=1.0)
 
     corner_checker=CornerChecker(dtype=filter_dtype)
-    mS_corner = corner_checker.filter_corners(image=img2)
+    mS_corner = corner_checker.filter_corners(image=img)
 
     mS = np.where(mS_corner > 0, mS, 0)
     mS = mS / np.nanmax(mS + 0.00000001)
@@ -77,6 +67,24 @@ def _detect_puzzles(img, min_width, curved, decoder):
     first_eigenvector_x  = h_d.f_xx - h_d.f_yy + root
     second_eigenvector_x = h_d.f_xx - h_d.f_yy - root
     both_eigenvectors_y  = -2 * h_d.f_xy
+
+    return mS, dot, sub_dot, first_eigenvector_x, second_eigenvector_x, both_eigenvectors_y
+
+
+def _detect_puzzles(img, min_width, curved, decoder):
+
+    global filter_dtype, h_d, corner_checker
+
+    assert filter_dtype in [np.float32, np.float64], 'Currently only float dtype is supported'
+
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY).astype(filter_dtype)
+    gray = gray/255
+
+# For low resolution images, there should be less blurring:
+#    img2 = cv2.filter2D(src=gray, ddepth=-1, kernel=np.array([[0,1,0],[1,1,1],[0,1,0]]))
+    img2 = cv2.GaussianBlur(gray, (3, 3), 1)
+
+    mS, dot, sub_dot, first_eigenvector_x, second_eigenvector_x, both_eigenvectors_y = _filter_image(img2)
 
     # Compute x-value and common y-value of first (positive) and second (negative) Eigen-vector
     ev1_x_at_max = np.fromiter(( first_eigenvector_x[dot[idx, 0], dot[idx, 1]] for idx in range(len(dot))),float)
